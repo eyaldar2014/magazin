2304408f48a4bf85b59f733c7f2be658d7a5eed0
Eyal Dar <eyaldar2014@gmail.com>
2021-04-17 13:06:39 +0300

    start design

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,31 +1,33 @@
 import react from 'react'
 
-const About =(props)=>{
+const About = (props) => {
 
 
 
     return <>
-        <div className={'about'}>
-
-            this is my first react project,
-
-            It focuses on a subject I love : cars.
-
+        <div className={'aboutContainer'}>
+            <div className={'about'}>
+                <h1>About</h1>
+                <br /><br />
+            this is my first react project, It focuses on a subject I love - cars.
+            <br /><br />
             This project contains few elements: well prepared site that reflects some functions for car owners and
             lovers, with full developed react structure including: smart components, roots, api uses, data save&handling,
             images&videos, form submit, controlled inputs, contact methods, awesome landing page with spinner, and smart
-            algorithm as the site core - the purpose of this site.
-
+            "algorithm" as the site core - the purpose of this site.
+            <br /><br />
             This smart algorithm helps car owners understanding car costs and choose options like saving money, getting
-            different car on equal price, and see proportions of different car companies. Also it shows one a psth to
+            different car on equal price, and see proportions of different car companies. Also it shows one a path to
             pocess his/hers dream car by making priorities.
-
+            <br /><br />
             In the future, this site can easily add well targeted shop, using users info and activity, with exact
             products for their specific car, preferation, need, and budget. as well as subscription for personal relevant
             information per user based on their car declaration as car owner and/or fan.
+            </div>
+
+            <div className='aboutImg'></div>
 
         </div>
-
     </>
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import react from 'react'
 import { Link } from "react-router-dom";
-
+import { FiHome ,  } from "react-icons/fi";
+import { BiCar , BiNews } from "react-icons/bi";
+import { GrInfo , GrPhone } from "react-icons/gr";
 
 const Navbar =(props)=>{
-
 
 
 
@@ -12,18 +13,18 @@ const Navbar =(props)=>{
 
             <ul className={'navbar'}>
                 <li>
-                    <Link to="/home">Home</Link>
+                    <Link to="/home" className="navItem"><FiHome /> Home</Link>
+                </li>                
+                <li>
+                    <Link to="/engine" className="navItem"><BiCar /> Engine</Link>
                 </li>
                 <li>
-                    <Link to="/engine">Engine</Link>
+                    <Link to="/magazine" className="navItem"><BiNews /> Magazine</Link>
                 </li>
                 <li>
-                    <Link to="/magazine">Magazine</Link>
+                    <Link to="/about" className="navItem"><GrInfo /> About</Link>
                 </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li><Link to="/contact">Contact</Link>
+                <li><Link to="/contact" className="navItem"><GrPhone /> Contact</Link>
                 </li>
             </ul>
 
